8ebeb2b7b7af1785b979201f79b4cf60298bd163
Matt Marra <matt.marranara@gmail.com>
2022-05-04 09:06:20 -0700

    Refactored components into respective files and put items in a flex box

---
+++ src/StateList.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import states from './states.json';
+
+export const StateList = () => {
+   const stateArray: string[] = Object.keys(states).sort();
+   const [selectedState, setSelectedState] = useState<Element>();
+
+   const findState = (stateID: string) => {
+      const selectedStateNode = document.querySelector(`#${stateID}`);
+      if (selectedStateNode != null) {
+         setSelectedState(selectedStateNode);
+      }
+      selectedStateNode?.classList.add('active');
+   };
+
+   const deselectState = () => {
+      selectedState?.classList.remove('active');
+      const emptyElement = document.createElement('div');
+      setSelectedState(emptyElement);
+   };
+
+   return (
+      <ul className='state-list'>
+         {stateArray.map((item: string, index: number) => {
+            return (
+               <li key={index}>
+                  <button onMouseDown={() => findState(item)} onMouseUp={deselectState}>
+                     {states[item]}
+                  </button>
+               </li>
+            );
+         })}
+      </ul>
+   );
+};
